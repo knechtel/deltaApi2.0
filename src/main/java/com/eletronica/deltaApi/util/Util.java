4126c9bfa424190c9b110ae170095619462a10ce
maiquelknechtel <maiquelknechtel@maiquelknechtels-MacBook-Pro.local>
2020-06-06 23:05:37 -0300

    Adicionando classe de utilitários.

--- src/main/java/com/eletronica/deltaApi/util/Util.java
+++ src/main/java/com/eletronica/deltaApi/util/Util.java
@@ -1,4 +1,32 @@
 package com.eletronica.deltaApi.util;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class Util {
+
+    public static Date toDate(String string){
+        Date date= null;
+        try {
+            date = new SimpleDateFormat("dd/MM/yyyy").parse(string);
+        } catch (ParseException e) {
+            System.out.println("123456789 1234567 -------------");
+            System.out.println(string);
+            System.out.println("MOSTRA");
+            e.printStackTrace();
+        }
+        return date;
+    }
+    public static  String dateToSring(Date dateString){
+        if(dateString==null){
+            return "";
+        }
+        Date date = Calendar.getInstance().getTime();
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd");
+        String strDate = dateFormat.format(dateString);
+        return  strDate;
+    }
 }
